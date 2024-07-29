23298383cb293bb51c21f2b29da2d4b856ed6695
mukul273 <msamak@gmail.com>
2024-07-29 05:54:43 -0400

    lesson 6th and comments added

--- README.md
+++ README.md
@@ -6,7 +6,10 @@
 4. `tsc {your file name}.ts` - to compile and make js out of ts file,
 5. `tsc {your file name}.ts -w` - to not need to compile for every time you make changes but -w (watch) attribute will monitor and make changes to js file automatically,
 6. Most files contain self explanatory code, Please try to uncomment the code for better understanding and try to compile and you should get an error why the code will not work and why it's commented,
-7. 
+7. `?` - desinates optional property,
+8. once declared the object, you have to set all the prorps, can't skip it unless there is a `?` character in the definition,
+9. `Objects` can be reassigned with different similar data types see [this](./fifth/fifth.ts),
+10. 
 
 
 ### References
--- fifth/fifth.js
+++ fifth/fifth.js
@@ -1,52 +1,33 @@
-let character: string = 'mario';
-let age: number;
-let isLoggedIn: boolean;
-
+var character = 'mario';
+var age;
+var isLoggedIn;
 // age = 'luigi';
 age = 30;
-
 // isLoggedIn = 25;
 isLoggedIn = true;
-
 // arrays
-let ninjas: string[] = [];
-
+var ninjas = [];
 ninjas.push('ryu');
 ninjas.push('chun-li');
 console.log(ninjas);
-
 // union types
-let mixed: (string|number|boolean)[] = [];
+var mixed = [];
 mixed.push('Chimper');
 mixed.push(false);
 mixed.push(20);
 console.log(mixed);
-
-
-let uid: string|number;
+var uid;
 uid = 'Chimper',
-uid=20;
+    uid = 20;
 //uid = false;
-
 console.log(uid);
-
 // objects
-let ninjaOne: object;
+var ninjaOne;
 ninjaOne = { name: 'Chimp yoshi', age: 30 };
-
 console.log(ninjaOne);
-
 ninjaOne: [];
 ninjaOne = ["Chimper Ding"];
-
 console.log(ninjaOne);
-let ninjaTwo: {
-  name: string,
-  age: number,
-  beltColour: string,
-  skills?: string
-};
-
+var ninjaTwo;
 ninjaTwo = { name: 'ken', age: 20, beltColour: 'black' };
-
 console.log(ninjaTwo);
