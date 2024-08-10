ff8a1d4da228d8eae90f2e46f0697ada92bfb08b
mukul273 <msamak@gmail.com>
2024-08-10 13:01:22 -0400

    015

---
+++ 015/src/app.ts
@@ -0,0 +1,66 @@
+//interfaces
+
+import { Invoice } from './classes/Invoice.js'
+
+export interface IsPerson {
+  name: string;
+  age?: number;
+  speak(a: string): void;
+  spend(a: number): number;
+}
+
+const me: IsPerson = {
+  name: 'Chimp',
+  age: 30,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log('I spent ', amount);
+    return amount;
+  },
+};
+
+console.log(me);
+me.speak('hello, world');
+
+const greetPerson = (person: IsPerson): void => {
+  console.log('hello ', person.name);
+}
+
+greetPerson(me);
+
+
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+
+// invOne.client = 'yoshi';
+// invOne.amount = 50;
+
+let invoices: Invoice[] = [];
+invoices.push(invOne)
+invoices.push(invTwo);
+
+invoices.forEach(inv => {
+  console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
+})
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children);
+
+// inputs
+const type = document.querySelector('#type') as HTMLInputElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+
+  console.log(
+    type.value, 
+    tofrom.value, 
+    details.value, 
+    amount.valueAsNumber
+  );
+});
